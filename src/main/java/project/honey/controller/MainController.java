73957debb95d3a0ed8fad179fa29f274f247b816
skek <skek9865>
2022-11-15 21:09:13 +0900

    수정

--- src/main/java/project/honey/controller/MainController.java
+++ src/main/java/project/honey/controller/MainController.java
@@ -5,10 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import project.honey.GlobalConst;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @Slf4j
@@ -16,8 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 public class MainController {
 
     @GetMapping("/")
-    public String main(HttpServletRequest request, Model model){
-        model.addAttribute("style", "~{main/main:: style}");
+    public String main(Model model){
         model.addAttribute("title", GlobalConst.title);
         model.addAttribute("cssDir", GlobalConst.cssDir);
         model.addAttribute("jsDir", GlobalConst.jsDir);
