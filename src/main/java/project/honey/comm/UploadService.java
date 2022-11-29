81c40c60a4b6b1d5cfb58ea14d66ed7f47d93cf8
ChanGenie <ckdwls0702@naver.com>
2022-11-29 09:56:55 +0900

    010101 - 파일 업로드 방식 변경

--- src/main/java/project/honey/comm/UploadService.java
+++ src/main/java/project/honey/comm/UploadService.java
@@ -5,19 +5,22 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.UUID;
 
 @Service
 public class UploadService {
 
-    public static void uploadFile(MultipartFile multipartFile, String path){
+    public static String uploadFile(MultipartFile multipartFile, String path){
+        String uuid = UUID.randomUUID().toString();
         String originalFilename = multipartFile.getOriginalFilename();
-        String filePath = path+"/"+originalFilename;
+        String filePath = path+"/"+uuid+"#"+originalFilename;
         File file = new File(filePath);
         try{
             multipartFile.transferTo(file);
         } catch(IOException e){
             e.printStackTrace();
         }
+        return filePath;
     }
 
 }
