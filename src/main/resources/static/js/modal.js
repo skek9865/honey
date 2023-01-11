38b980265363705b2c3d84e3e4ae617ad8f984d4
skek9865 <83535254+skek9865@users.noreply.github.com>
2023-01-11 22:33:20 +0900

    Delete src/main/resources/static/js directory

--- src/main/resources/static/js/modal.js
+++
@@ -1,8 +0,0 @@
-//modal Ã³¸® script
-$(document).ready( function() {
-	$(opener.document).find('#loading').show();
-});
-$(window).on( "unload", function( event, ui ) {
-	$(opener.document).find('#loading').hide();
-});
- 
